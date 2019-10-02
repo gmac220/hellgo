c51cd5c59a84e8c71c9a026b5ddad9b685722c8d
gmac220 <godfreymacasero@gmail.com>
2019-10-02 13:59:54 -0500

    finished usage feature

--- main.go
+++ main.go
@@ -5,22 +5,16 @@ import (
 	"os"
 )
 
-func translate(input string) string {
-	translations := make(map[string]string)
-	translations["en"] = "Hello"
-	translations["es"] = "Hola"
-	translations["de"] = "Guten Tag"
-	translations["fr"] = "Bonjour"
-	translations["ch"] = "Ni Hao"
+var translations map[string]string
 
+func translate(input string) string {
 	return translations[input]
 }
 
 func argswitch(input []string) string {
 	var locale string
 	if len(input) == 1 {
-		fmt.Printf("What language do you want?: ")
-		fmt.Scanln(&locale)
+		help()
 	} else {
 		locale = os.Args[1]
 	}
@@ -28,20 +22,26 @@ func argswitch(input []string) string {
 	return locale
 }
 
+func help() {
+	fmt.Println("usage: hellgo args1")
+	fmt.Println("args: ")
+
+	for index := range translations {
+		fmt.Println(index)
+	}
+
+	os.Exit(0)
+}
+
 func main() {
+	translations = make(map[string]string)
+	translations["en"] = "Hello"
+	translations["es"] = "Hola"
+	translations["de"] = "Guten Tag"
+	translations["fr"] = "Bonjour"
+	translations["ch"] = "Ni Hao"
+
 	locale := argswitch(os.Args)
-	var available = []string{"English in en", "Spanish in es", "German in de", "French in fr", "Chinese in ch"}
-
-	fmt.Println("Select a language")
-
-	for i := 0; i < len(available); i++ {
-		fmt.Println(available[i])
-	}
-
-	if len(locale) != 2 {
-		fmt.Printf("Please input only two letters: ")
-		fmt.Scanln(&locale)
-	}
 
 	output := translate(locale)
 
